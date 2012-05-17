881327616038cbb1f8d1a28581a0c49e9e922c30
Evolvinx Developer <evolvinx.developer@gmail.com>
2012-05-18 03:55:19 +0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsDemo::Application.config.secret_token = '2f1fee1d4137409fd3aa6e3615b8436f687c2c3c83243d4a48d8b4c28ae061c75f3f55ea5d20c09d1d57e995abff1ccb5a110f86f31a10fd34f8bd175fa75666'
